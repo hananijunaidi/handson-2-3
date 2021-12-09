81f7a6866aa96d20803a6387197ff585de9ba230
hananijunaidi <93896486+hananijunaidi@users.noreply.github.com>
2021-12-10 01:49:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-  $my_string = "Hello Guys, I am Nurhanani Junaidi, the Cloud Computer expert! Please go to my GitHub account https://github.com/hananijunaidi "; 
-  echo $my_string;
+$my_string = "Hello Guys, I am Nurhanani Junaidi, the Cloud Computer expert! Please go to my GitHub account https://github.com/hananijunaidi "; 
+echo $my_string;
 ?>
